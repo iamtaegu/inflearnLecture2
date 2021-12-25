551fb003ed38833869552b8db95bc701c0e9e1e1
Taegu <gmdals45@gmail.com>
2021-12-25 15:34:55 +0900

    2021.12.25

---
+++ jpashop(1)/src/main/java/com/example/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,24 @@
+package com.example.jpabook.jpashop.repository;
+
+import com.example.jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+
+}
