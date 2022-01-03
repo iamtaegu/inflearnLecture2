3c4293641d926be7386229d8d8b1fff279763151
Taegu <gmdals45@gmail.com>
2022-01-03 21:01:55 +0900

    2021/01/03

--- jpashop/src/main/java/jpabook/jpashop/controller/ReactController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/ReactController.java
@@ -3,13 +3,19 @@ package jpabook.jpashop.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 @RestController
 public class ReactController {
 
     @GetMapping("/react/hello")
-    public String hello() {
-        return "현재 시간은 " + new Date() + "입니다.";
+    public List<String> hello()
+    {
+        return Arrays.asList("현재 시간",  new Date().toString()+"입니다.");
     }
+
+    @GetMapping("hello")
+    public List<String> hello2() { return Arrays.asList("현재 시간",  new Date().toString()+"입니다."); }
 }
