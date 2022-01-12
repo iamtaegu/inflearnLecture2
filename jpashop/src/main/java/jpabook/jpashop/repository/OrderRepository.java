30f14d0e242b28934c4ef69ed570a268645ddc4a
Taegu <gmdals45@gmail.com>
2022-01-12 21:24:32 +0900

    2022.01.12

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -78,4 +78,30 @@ public class OrderRepository {
                 .getResultList();
     }
 
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                // JPA에서 distinct 키워드를 sql에 distinct 기능
+                // + Order.order_id가 중복되면 제거해줌
+                "select distinct o from Order o" +
+                        " join fetch o.member m " +
+                        " join fetch o.delivery d" +
+                        " join fetch o.orderItems oi" +
+                        " join fetch oi.item i", Order.class)
+                .getResultList();
+                // fetch; applying in memory!
+                /*.setFirstResult(1)
+                .setMaxResults(100)
+                .getResultList();*/
+
+    }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m " +
+                        " join fetch o.delivery d ", Order.class
+                ).setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
 }
