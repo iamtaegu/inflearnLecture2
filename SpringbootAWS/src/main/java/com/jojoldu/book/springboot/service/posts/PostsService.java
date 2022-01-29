e7a40b9c5aaa8b0f88730cd14c15bd5798cb4e04
Taegu <gmdals45@gmail.com>
2022-01-29 17:51:25 +0900

    2022-01-29

--- SpringbootAWS/src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
+++ SpringbootAWS/src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
@@ -1,6 +1,9 @@
 package com.jojoldu.book.springboot.service.posts;
 
+import com.jojoldu.book.springboot.domain.posts.Posts;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import com.jojoldu.book.springboot.web.PostsUpdateRequestDto;
+import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,7 +17,27 @@ public class PostsService {
 
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
-        return postsRepository.save(requestDto.toEntity().getId());
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    /**
+     * JPA의 영속성 컨텍스트(=엔티티를 영구 저장하는 환경)로 DB에 UPDATE 쿼리를 날리는 부분이 없음
+     */
+    @Transactional
+    public Long update (Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        return new PostsResponseDto(entity);
     }
 
 }
